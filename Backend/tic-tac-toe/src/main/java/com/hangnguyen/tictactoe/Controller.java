d06742ce3a9b79ffc942695716e618b46890d5bc
JaylynnValensa <thuyhangnguyen190298@gmail.com>
2023-10-28 16:38:18 +0300

    first commit

--- Backend/tic-tac-toe/src/main/java/com/hangnguyen/tictactoe/Controller.java
+++ Backend/tic-tac-toe/src/main/java/com/hangnguyen/tictactoe/Controller.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.hangnguyen.tictactoe.model.UpdateCellRequest;
 @CrossOrigin(origins = "*")
